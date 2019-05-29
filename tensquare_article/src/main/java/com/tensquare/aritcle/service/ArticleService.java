bdfbd64f2e1d06f26104b1d8d2de795b0cbcd3a6
ex_zhuweijia <2277034683@qq.com>
2019-05-29 19:30:51 +0800

    update .gitignore

--- tensquare_article/src/main/java/com/tensquare/aritcle/service/ArticleService.java
+++
@@ -1,8 +0,0 @@
-package com.tensquare.aritcle.service;
-
-public interface ArticleService {
-
-    boolean updateArticleState(String articleId);
-
-    boolean updateThumbup(String articleId, int thumbup, boolean isAdd);
-}
